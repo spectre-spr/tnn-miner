b2b01bcea7d7d4c8d3452d4b5feabb0efd0cbc27
herniy <2667004988@qq.com>
2024-08-18 12:46:48 +0000

    update win

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,36 +1,10 @@
- param (
-    [string]$version = ""
- )
-
-# Create build directory
-if (!(Test-Path .\build)) {
-    New-Item -ItemType Directory -Path .\build
-}
-
+set sevenzexe="C:\Program Files\7-Zip\7z.exe"
+mkdir .\build
 pushd .\build
 
-# Check if clang is in PATH, otherwise add it
-if (!(Get-Command clang -ErrorAction SilentlyContinue)) {
-    $env:PATH = "c:\mingw64\bin;" + $env:PATH
-}
-
-# Check again if clang is in PATH, print directory contents if not found
-if (!(Get-Command clang -ErrorAction SilentlyContinue)) {
-    Get-ChildItem c:\mingw64\bin\
-    Write-Output "Build will fail. Cannot find clang.exe"
-}
-
-set-variable -name "VER_SETTING" -Value ""
-if (-not $version -eq "") {
-  set-variable -name "VER_SETTING" -value "-DTNN_VERSION=$version"
-}
-
-# Determine CMake command based on argument
-#& c:\mingw64\bin\cmake.exe $VER_SETTING -DCMAKE_MAKE_PROGRAM=c:/mingw64/bin/ninja.exe --debug-trycompile -G "Ninja" ..
-& c:\mingw64\bin\cmake.exe $VER_SETTING --debug-trycompile --trace-expand --trace-redirect=./asdf.txt -G "Ninja" ..
-
-# Run Ninja build
-& c:\mingw64\bin\ninja.exe
-
-# Return to original directory
+dir c:\mingw64\bin\
+c:\mingw64\bin\cmake.exe ..
+c:\mingw64\bin\ninja.exe
+move Tnn-miner.exe Tnn-miner*
+%sevenzexe% a ..\tnn-miner-win64.zip Tnn-miner-win-x64.exe
 popd
