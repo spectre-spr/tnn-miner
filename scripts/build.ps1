40589ef619aece8ef54791f2356713a29a0146fa
herniy <2667004988@qq.com>
2024-08-18 12:56:09 +0000

    修复bug

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -5,6 +5,6 @@ pushd .\build
 dir c:\mingw64\bin\
 c:\mingw64\bin\cmake.exe ..
 c:\mingw64\bin\ninja.exe
-move Tnn-miner.exe Tnn-miner*
+move Tnn-miner* Tnn-miner-win-x64.exe
 %sevenzexe% a ..\tnn-miner-win64.zip Tnn-miner-win-x64.exe
 popd
