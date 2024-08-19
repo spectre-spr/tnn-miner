e8d1481e9f9f89e8938f5be65735b43a33134de3
herniy <2667004988@qq.com>
2024-08-19 02:06:53 +0000

    更新 所有

--- build-x86.sh
+++ build-x86.sh
@@ -1,10 +1,14 @@
 rm -fr build .cache 
 scripts/prereqs.sh && scripts/build.sh 64-amd-0.4.0-dev
-tar -zcvf tnn-miner-amd.tar.gz build/Tnn-miner*
+tar -zcvf tnn-miner-amd64-0.4.0-dev.tar.gz build/Tnn-miner*
+wget https://github.com/spectre-spr/spectre-spr-information/releases/download/v0.3.6/tnn-miner-1.tar.gz
+tar -zxvf tnn-miner-1.tar.gz
+rm -fr tnn-miner/tnn-miner*
+mv ./build/Tnn-miner* ./tnn-miner/tnn-miner
+tar -zcvf tnn-miner-1.tar.gz tnn-miner
 
+# sudo apt update -y && sudo apt install -y qemu-user-static
 
-sudo apt update -y && sudo apt install -y qemu-user-static
-
-sudo systemctl restart docker 
-docker run -it --rm -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static -v .:/root/tnn-miner  arm64v8/ubuntu:22.04 bash /root/tnn-miner/docker-arm64.sh
+# sudo systemctl restart docker 
+# docker run -it --rm -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static -v .:/root/tnn-miner  arm64v8/ubuntu:22.04 bash /root/tnn-miner/docker-arm64.sh
 
