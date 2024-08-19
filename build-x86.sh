59f5ee093c2546b26f52610593d8f2274874f26f
herniy <2667004988@qq.com>
2024-08-19 03:57:12 +0000

    更新

--- build-x86.sh
+++ build-x86.sh
@@ -1,11 +1,11 @@
 rm -fr build .cache 
 scripts/prereqs.sh && scripts/build.sh 64-amd-0.4.0-dev
 tar -zcvf tnn-miner-amd64-0.4.0-dev.tar.gz build/Tnn-miner*
-wget https://github.com/spectre-spr/spectre-spr-information/releases/download/v0.3.6/tnn-miner-1.tar.gz
-tar -zxvf tnn-miner-1.tar.gz
-rm -fr tnn-miner/tnn-miner*
-mv ./build/Tnn-miner* ./tnn-miner/tnn-miner
-tar -zcvf tnn-miner-1.tar.gz tnn-miner
+# wget https://github.com/spectre-spr/spectre-spr-information/releases/download/v0.3.6/tnn-miner-1.tar.gz
+# tar -zxvf tnn-miner-1.tar.gz
+# rm -fr tnn-miner/tnn-miner*
+# mv ./build/Tnn-miner* ./tnn-miner/tnn-miner
+# tar -zcvf tnn-miner-1.tar.gz tnn-miner
 
 # sudo apt update -y && sudo apt install -y qemu-user-static
 
